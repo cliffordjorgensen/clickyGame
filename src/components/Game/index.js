80268334beba2c5ec2533b8ee93b13cb641235c7
cliffordjorgensen <cliffordjorgensen@gmail.com>
2019-08-03 14:46:01 -0700

    working on Game index.js

---
+++ src/components/Game/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Nav from "../Nav";
+import Header from "../Header";
+import Container from "../Container";
+import ClickItem from "../ClickItem";
+import Footer from  "../Footer";
+import data from "../../data.json";
+
+export class Game extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Game
